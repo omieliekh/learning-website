47e785a9478cabc3dd8effd6637a73212094cffe
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2015-06-22 23:27:07 +0200

    Added test actions

--- project/src/app/test-autocode/test-autocode.js
+++ project/src/app/test-autocode/test-autocode.js
@@ -38,24 +38,60 @@ angular.module( 'testAutocode', [
 
 	$scope.startRecord = function(){
 		TestAutocodeService.startRecord();
-
-
 	};
 
 	$scope.stopRecord = function(){
 		TestAutocodeService.stopRecord();
-
-
 	};
 
 
-	$scope.captureEvent = function(e){
-		
+	// $scope.captureEvent = function(e){
+	// 	var value = String.fromCharCode(e.keyCode);
+	// 	console.log('e: ', e, ' , value: ', value);
+	// };
 
-		var value = String.fromCharCode(e.keyCode);
+	$scope.aceLoaded = function(editor) {
+		// _editor.setReadOnly(true);
+		console.log('aceLoaded. editor: ', editor);
 
-		console.log('e: ', e, ' , value: ', value);
+		window.myEditor = editor;
+
+		// myEditor.commands.exec("selectall", myEditor)
+
+		// editor.commands.addCommand({
+		// 	name: "inserttext",
+		// 	exec: function(editor, args, request) {
+		// 	    var lang = require("ace/lib/lang");
+
+		// 	    editor.insert(lang.stringRepeat(args.text || "", args.times || 1));
+
+		// 	    if (autocompleting) {
+
+		// 	        var command = editor.commands.commands['autocomplete'];
+
+		// 	        command.exec(editor);
+		// 	    }
+		// 	}
+		// });
+		// editor.commands.addCommand({
+		// 	name: "triggerAutocomplete",
+		// 	bindKey: {
+		// 	    win: "Ctrl-Space",
+		// 	    mac: "Ctrl-Space",
+		// 	    sender: "editor"
+		// 	},
+		// 	exec: function(editor, args, request) {
+		// 	    setTimeout(function() {
+		// 	        generateAdvice(AdviceOn.COMMAND);
+		// 	    }, 0);
+		// 	}
+		// });
 	};
+
+	$scope.aceChanged = function(e) {
+		console.log('aceChanged. e[0]: ', e[0]);
+	};
+
 
 
 }])
